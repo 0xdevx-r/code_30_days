02daa29032c0779e3d59076a3deb3a39af8823f8
newbeetoveteran <rahul.singh04@zohomail.in>
2026-09-20 19:29:26 +0530

    defining function with a placeholder/function parameter

---
+++ function_parameter.py
@@ -0,0 +1,6 @@
+# defining a function with function parameters
+
+def say_hello(name):    # name as parameter/placeholder
+    print("Hello", name)
+
+say_hello("Max!")        # Max as argument/value
