eb4fd719dced72a001d9c724dbdc493ab438b520
newbeetoveteran <rahul.singh04@zohomail.in>
2026-09-20 20:02:22 +0530

    functions calling with parameter

---
+++ function_parameter_3_name.py
@@ -0,0 +1,8 @@
+# write a greeting function which greets people with their name
+
+def greet_user(name):
+    print("Hello", name)
+
+greet_user("Maximus.")
+greet_user("Megatron.")
+greet_user("BumbleBee.")
